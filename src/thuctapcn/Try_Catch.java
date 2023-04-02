2586b82ce9ce62b64590a16695443189179425c5
tuanthanh1011 <92152534+tuanthanh1011@users.noreply.github.com>
2023-04-02 09:21:27 +0700

    Update Try_Catch.java

--- src/thuctapcn/Try_Catch.java
+++ src/thuctapcn/Try_Catch.java
@@ -3,40 +3,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package bai1_javatongquan;
+package javatongquan;
 import java.util.Scanner;
 /**
  *
  * @author tuant
  */
 public class Bai2_GiaiPTBac2 {
-   public static void main(String[] args) {
-    Scanner sn = new Scanner(System.in);
-    float a, b, c;
-    double x1, x2;
-    System.out.print("Nhap he so a: ");
-    a = sn.nextFloat();
-    System.out.print("Nhap he so b: ");
-    b = sn.nextFloat();
-    System.out.print("Nhap he so c: ");
-    c = sn.nextFloat();
-    if(a == 0) {
-        x1 = -b/c;
+   public class Main {
+  public static void main(String[] args) {
+    try {
+      int[] myNumbers = {1, 2, 3};
+      System.out.println(myNumbers[10]);
+    } catch (Exception e) {
+      System.out.println("Something went wrong.");
+    } finally {
+      System.out.println("The 'try catch' is finished.");
     }
-    else {
-        double delta = b*b - 4*a*c;
-        if (delta > 0) {
-            x1 = (-b + Math.sqrt(delta)) / 2*a;
-            x2 = (-b - Math.sqrt(delta)) / 2*a;
-            System.out.println("Phuong trinh co 2 nghiem phan biet");
-        }
-        else if (delta < 0) {
-            x1 = x2 = -b/2*a;
-            System.out.println("Phuong trinh vo nghiem");
-        }else {
-            System.out.println("Phuong trinh co nghiem kep");
-        }
-    }
-   }
-    
+  }
 }
+    
+
