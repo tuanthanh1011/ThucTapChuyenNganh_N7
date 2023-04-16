22257e6575a7b4182e4fd5817eb82717f10a0fa4
An Viet Computer <An Viet Computer@DESKTOP-V3ASPIG>
2023-04-16 10:12:15 +0700

    Dao Thi Thao Upload

--- src/thuctapcn/Tuan6/vidu4/Nguoi.java
+++ src/thuctapcn/Tuan6/vidu4/Nguoi.java
@@ -1,3 +1,4 @@
+<<<<<<< Upstream, based on origin/master
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -70,3 +71,59 @@ public abstract class Nguoi implements Comparable<Nguoi> {
     }
 }
 
+=======
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thuctapcn.Tuan6.vidu4;
+
+/**
+ *
+ * @author An Viet Computer
+ */
+public class Nguoi {
+    private String ten;
+    private int tuoi;
+    private char gioiTinh;
+    public Nguoi(){
+        
+    }
+    public Nguoi(String ten, int tuoi, char gioiTinh){
+        this.ten = ten;
+        this.tuoi = tuoi;
+        this.gioiTinh = gioiTinh;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public int getTuoi() {
+        return tuoi;
+    }
+
+    public char getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public void setTuoi(int tuoi) {
+        this.tuoi = tuoi;
+    }
+
+    public void setGioiTinh(char gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    @Override
+    public String toString() {
+        return "ten=" + ten + ", tuoi=" + tuoi + ", gioiTinh=" + gioiTinh + " ";
+    }
+    
+}
+>>>>>>> 24b9b51 Dao Thi Thao Upload
